4508dd6cf07dd359232d1d1ffb30cce6a372ca57
BatiSileshi <batisileshi93@gmail.com>
2024-11-22 22:59:23 +0300

    allowed all

--- scan/urls.py
+++ scan/urls.py
@@ -1,6 +1,6 @@
-# from django.urls import path
-# from .views import extract_prescription
+from django.urls import path
+from . import views
 
-# urlpatterns = [
-#     path("extract-prescription/", extract_prescription, name="extract_prescription"),
-# ]
+urlpatterns = [
+    path('extract-prescription/', views.extract_prescription, name='extract_prescription'),
+]
